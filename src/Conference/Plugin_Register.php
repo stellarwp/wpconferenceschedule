43779979414b7d2ee09b47cb16c215635d79afe5
jesseeproductions <info@jesseeproductions.com>
2023-08-29 21:27:20 -0400

    TEC-4882 - update plugin version

--- src/Conference/Plugin_Register.php
+++ src/Conference/Plugin_Register.php
@@ -29,7 +29,7 @@ class Plugin_Register extends Abstract_Plugin_Register {
 	 *
 	 * @var string
 	 */
-	public const VERSION  = '1.0.0';
+	public const VERSION  = Plugin::VERSION;
 
 	/**
 	 * Configures the base_dir property which is the path to the plugin bootstrap file.
