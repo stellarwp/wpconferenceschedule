380067a7ee3caa82d8159159d7a76f8e18941cb6
jesseeproductions <info@jesseeproductions.com>
2023-08-29 20:58:53 -0400

    TEC-4882 - remove common and load stellar libraries from composer

---
+++ src/Exceptions/Container_Exception.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace TEC\Conference\Exceptions;
+
+use RuntimeException;
+
+/**
+ * Class Container_Exception.
+ *
+ * @since   TBD
+ *
+ * @package TEC\Conference\Exceptions
+ */
+class Container_Exception extends RuntimeException {
+	// Intentionally empty.
+}
