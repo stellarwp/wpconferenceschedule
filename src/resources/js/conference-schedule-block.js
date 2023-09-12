dd880b28d5a8c16a230575c35e3269615d5b58e7
jesseeproductions <info@jesseeproductions.com>
2023-09-12 09:44:19 -0400

    TEC-4882 - Update block editor script to latest standards remove previous plugin coding

--- src/resources/js/conference-schedule-block.js
+++ src/resources/js/conference-schedule-block.js
@@ -1,37 +1,24 @@
-(function( wp ) {
+( ( wp ) => {
 
-	var registerBlockType = wp.blocks.registerBlockType;
-	var InspectorControls = wp.editor.InspectorControls;
-	var TextControl = wp.components.TextControl;
-	var SelectControl = wp.components.SelectControl;
-	var CheckboxControl = wp.components.CheckboxControl;
-	var withState = wp.compose.withState;
-	var el = wp.element.createElement;
-	const ServerSideRender = wp.serverSideRender;
-	var DatePicker = wp.components.DateTimePicker;
-	var __ = wp.i18n.__;
-	//var RichText = wp.editor.RichText;
-	//var AlignmentToolbar = wp.editor.AlignmentToolbar;
-	//var BlockControls = wp.editor.BlockControls;
+	const { registerBlockType } = wp.blocks;
+	const { InspectorControls, ServerSideRender } = wp.editor;
+	const { SelectControl, CheckboxControl, DateTimePicker } = wp.components;
+	const apiFetch = wp.apiFetch;
+	const { createElement: el } = wp.element;
+	const { __ } = wp.i18n;
 
-	function wpcs_dateFormatted( date ) {
-		if ( date == null ) {
-			var date = new Date();
-		} else {
-			var date = new Date( date );
-		}
-		var dd = String( date.getDate() ).padStart( 2, '0' );
-		var mm = String( date.getMonth() + 1 ).padStart( 2, '0' ); //January is 0!
-		var yyyy = date.getFullYear();
+	const wpcs_dateFormatted = ( date = new Date() ) => {
+		let dd = String( date.getDate() ).padStart( 2, '0' );
+		let mm = String( date.getMonth() + 1 ).padStart( 2, '0' ); //January is 0!
+		let yyyy = date.getFullYear();
 
-		date = yyyy + '-' + mm + '-' + dd;
-		return date;
-	}
+		return `${yyyy}-${mm}-${dd}`;
+	};
 
-	const trackTermsArray = [];
-	wp.apiFetch( {path: "/wp/v2/session_track"} ).then( posts => {
-		posts.forEach( function( val, key ) {
-			trackTermsArray.push( {id: val.id, name: val.name, slug: val.slug} );
+	let trackTermsArray = [];
+	apiFetch( { path: "/wp/v2/session_track" } ).then( posts => {
+		posts.forEach( ( val, key ) => {
+			trackTermsArray.push( { id: val.id, name: val.name, slug: val.slug } );
 		} );
 	} );
 
@@ -40,133 +27,104 @@
 		icon: 'schedule',
 		category: 'common',
 		supports: {
-			align: true,
-			align: ['wide', 'full']
+			align: [ 'wide', 'full' ]
 		},
 		attributes: {
-			date: {type: 'string', default: wpcs_dateFormatted( null )},
-			color_scheme: {type: 'string', default: 'light'},
-			layout: {type: 'string', default: 'table'},
-			row_height: {type: 'string', default: 'match'},
-			session_link: {type: 'string', default: 'permalink'},
-			tracks: {type: 'string', default: null}
+			date: { type: 'string', default: wpcs_dateFormatted() },
+			color_scheme: { type: 'string', default: 'light' },
+			layout: { type: 'string', default: 'table' },
+			row_height: { type: 'string', default: 'match' },
+			session_link: { type: 'string', default: 'permalink' },
+			tracks: { type: 'string', default: null }
 		},
 
-		edit: function( props ) {
-			var attributes = props.attributes;
-			var setAttributes = props.setAttributes;
-			//var setState = props.setState;
-			//var status = props.status;
+		edit: ( props ) => {
+			const { attributes, setAttributes } = props;
+			const { date, color_scheme, layout, row_height, session_link, tracks } = attributes;
+			let tracksArray = tracks ? tracks.split( ',' ) : [];
 
-			var date = props.attributes.date;
-			var color_scheme = props.attributes.color_scheme;
-			var layout = props.attributes.layout;
-			var row_height = props.attributes.row_height;
-			var session_link = props.attributes.session_link;
-			var tracks = props.attributes.tracks;
-			if ( tracks != null ) {
-				var tracksArray = tracks.split( ',' );
-			} else {
-				var tracksArray = [];
-			}
-
-			trackCheckboxes = [];
-			for ( i = 0; i < trackTermsArray.length; i++ ) {
-				if ( i == 0 ) {
-					var heading = 'Tracks';
-				} else {
-					var heading = null;
-				}
-				trackCheckboxes.push(
-					el( CheckboxControl, {
-						key: trackTermsArray[i].slug,
-						label: trackTermsArray[i].name,
-						name: 'tracks[]',
-						value: trackTermsArray[i].slug,
-						checked: tracksArray.includes( trackTermsArray[i].slug ),
-						heading: heading,
-						onChange: function( e ) {
-							var track = event.target.value;
-							var index = tracksArray.indexOf( track );
-							if ( index > -1 ) {
-								tracksArray.splice( index, 1 );
-							} else {
-								tracksArray.push( track );
-							}
-							setAttributes( {tracks: tracksArray.join()} );
+			const trackCheckboxes = trackTermsArray.map( ( term, index ) => {
+				return el( CheckboxControl, {
+					key: term.slug,
+					label: term.name,
+					name: 'tracks[]',
+					value: term.slug,
+					checked: tracksArray.includes( term.slug ),
+					heading: index === 0 ? 'Tracks' : null,
+					onChange: ( e ) => {
+						const track = e.target.value;
+						const index = tracksArray.indexOf( track );
+						if ( index > -1 ) {
+							tracksArray.splice( index, 1 );
+						} else {
+							tracksArray.push( track );
 						}
-					} ),
-				);
-			}
+						setAttributes( { tracks: tracksArray.join() } );
+					}
+				} );
+			} );
 
 			return [
 				el( ServerSideRender, {
 					block: "wpcs/schedule-block",
-					attributes: props.attributes
+					attributes: attributes,
+					key: 'server-side-render'
 				} ),
-				el( InspectorControls, {},
-					el( DatePicker, {
-							currentDate: date,
-							locale: 'en',
-							onChange: function( value ) {
-								setAttributes( {date: wpcs_dateFormatted( value )} );
-							},
-							selected: date
-						}
-					),
+				el( InspectorControls, { key: 'inspector-controls' },
+					el( DateTimePicker, {
+						currentDate: date,
+						locale: 'en',
+						onChange: ( value ) => setAttributes( { date: wpcs_dateFormatted( new Date( value ) ) } ),
+						selected: date,
+						key: 'date-picker'
+					} ),
 					el( SelectControl, {
 						label: 'Color Scheme',
 						value: color_scheme,
 						options: [
-							{value: 'light', label: 'Light'},
-							{value: 'dark', label: 'Dark'},
+							{ value: 'light', label: 'Light' },
+							{ value: 'dark', label: 'Dark' },
 						],
-						onChange: function( value ) {
-							setAttributes( {color_scheme: value} );
-						}
+						onChange: ( value ) => setAttributes( { color_scheme: value } ),
+						key: 'color-scheme-select'
 					} ),
 					el( SelectControl, {
 						label: 'Layout',
 						value: layout,
 						options: [
-							{value: 'table', label: 'Table'},
-							{value: 'grid', label: 'Grid'},
+							{ value: 'table', label: 'Table' },
+							{ value: 'grid', label: 'Grid' },
 						],
-						onChange: function( value ) {
-							setAttributes( {layout: value} );
-						}
+						onChange: ( value ) => setAttributes( { layout: value } ),
+						key: 'layout-select'
 					} ),
 					el( SelectControl, {
 						label: 'Row height',
 						value: row_height,
 						options: [
-							{value: 'match', label: 'Match'},
-							{value: 'auto', label: 'Auto'},
+							{ value: 'match', label: 'Match' },
+							{ value: 'auto', label: 'Auto' },
 						],
-						onChange: function( value ) {
-							setAttributes( {row_height: value} );
-						}
+						onChange: ( value ) => setAttributes( { row_height: value } ),
+						key: 'row-height-select'
 					} ),
 					el( SelectControl, {
 						label: 'Session Link',
 						value: session_link,
 						options: [
-							{value: 'permalink', label: 'Permalink'},
-							{value: 'anchor', label: 'Anchor'},
-							{value: 'none', label: 'None'},
+							{ value: 'permalink', label: 'Permalink' },
+							{ value: 'anchor', label: 'Anchor' },
+							{ value: 'none', label: 'None' },
 						],
-						onChange: function( value ) {
-							setAttributes( {session_link: value} );
-						}
+						onChange: ( value ) => setAttributes( { session_link: value } ),
+						key: 'session-link-select'
 					} ),
 					trackCheckboxes,
 				),
 			];
 		},
 
-		save: function( props ) {
-			return null
-		}
+		save: () => null
 
 	} );
-}( window.wp ));
+} )( window.wp );
